3b9ffa961c06e71ce129eeee0746bd6951a1c35a
Hridoykhan4 <hridoykhan148385@gmail.com>
2026-05-14 17:04:16 +0600

    last day today

---
+++ server2/index.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const { GoogleGenAI } = require("@google/genai");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const ai = new GoogleGenAI(process.env.GEMINI_API_KEY);
+app.use(express.urlencoded({extended: true}))
+app.use((req, res, method) => {
+    
+})
+const form = `
+    <form method="POST" action="/prompt">
+    <textarea name="prompt" id="prompt></textarea>
+    <button type="submit">Generate</button>
+    </form>
+`;
+
+// Generate Reply (1)
+app.get("/test-ai", async (req, res) => {
+    res.send(form)
+});
+
+app.post("/prompt", async (req, res) => {
+  const { prompt } = req?.query;
+  // if(!prompt) return res.status(400).send({message: "Please provide some text"})
+  // const result = await ai.models.generateContent({
+  //   model: "gemini-2.5-flash",
+  //   contents: prompt
+  // });
+  res.send(result.text);
+});
+
+// Define a route for the home page
+app.get("/", (req, res) => {
+  res.send({ message: "Power of AI" });
+});
+
+// Start the server and listen for requests
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
