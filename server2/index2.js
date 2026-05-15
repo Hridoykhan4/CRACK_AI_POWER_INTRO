2d6b826ce527900a0c1c3635998e1d157e739103
Hridoykhan4 <hridoykhan148385@gmail.com>
2026-05-15 14:58:01 +0600

    changes updated

--- server2/index2.js
+++ server2/index2.js
@@ -1,33 +1,38 @@
 require("dotenv").config();
-const { GoogleGenAI } = require("@google/genai");
 const express = require("express");
 const app = express();
+const { GoogleGenAI } = require("@google/genai");
 const port = process.env.PORT || 5000;
 const ai = new GoogleGenAI(process.env.GEMINI_API_KEY);
-app.use(express.urlencoded({extended: true}))
-app.use((req, res, method) => {
-    
-})
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url} at ${new Date().toLocaleTimeString()}`);
+  next();
+});
 const form = `
     <form method="POST" action="/prompt">
-    <textarea name="prompt" id="prompt></textarea>
+    <textarea name="prompt" id="prompt"></textarea>
     <button type="submit">Generate</button>
     </form>
 `;
 
 // Generate Reply (1)
 app.get("/test-ai", async (req, res) => {
-    res.send(form)
+  res.send(form);
 });
 
 app.post("/prompt", async (req, res) => {
-  const { prompt } = req?.query;
-  // if(!prompt) return res.status(400).send({message: "Please provide some text"})
-  // const result = await ai.models.generateContent({
-  //   model: "gemini-2.5-flash",
-  //   contents: prompt
-  // });
-  res.send(result.text);
+  let { prompt } = req?.body;
+  if(!prompt) return res.status(400).send({message: "Please provide some text"})
+  const result = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: prompt , 
+    config: {
+      responseMimeType: 'application/json'
+    }
+  });
+  const responseText = result.text
+  res.send({data: JSON.parse(responseText)});
 });
 
 // Define a route for the home page
